7d8a3844d500ace961dbbcce3509ed0e32b5b388
Alejandro Trujano <alejandrotrujanoc0@gmail.com>
2026-02-12 21:26:24 -0600

    feature : Implement new service (#2)
    Why is change necessary?
    - There were no services to search, update, and delete tasks
    What changes were made?
    - Task update service
    - Task delete service
    - Methods were added to return all tasks as well as to filter by status (todo, done, in-progress)
    - Methods were added to update the status of a task

--- cmd/main.go
+++ cmd/main.go
@@ -5,6 +5,8 @@ import (
 	"os"
 	"strings"
 	"task_tracker/internal/service"
+
+	"github.com/google/uuid"
 )
 
 func main() {
@@ -31,6 +33,97 @@ func main() {
 
 		fmt.Printf("Task successful %s\n", descripcion)
 		break
+	case "update", "-u":
+		if len(args) < 3 {
+			fmt.Println("Error: Correct usage: update <id> <new description>")
+			return
+		}
+
+		idInput := args[1]
+		nuevaDesc := strings.Join(args[2:], " ")
+
+		success, msg := service.TaskUpdate(nuevaDesc, "", uuid.MustParse(idInput))
+		if !success {
+			fmt.Printf("%s\n", msg)
+		} else {
+			fmt.Printf("%s\n", msg)
+		}
+		break
+	case "list", "-l":
+
+		var result []service.RegisterTask
+		var errStr string
+
+		if len(args) == 2 {
+			status := args[1]
+			result, errStr = service.TaskByStatus(status)
+
+		} else {
+			result, errStr = service.TaskAll()
+		}
+
+		if errStr != "" {
+			fmt.Printf("Error: %s\n", errStr)
+			break
+		}
+
+		if len(result) == 0 {
+			fmt.Println("No tasks registered.")
+			break
+		}
+
+		for _, t := range result {
+			fmt.Printf("-----------------------------------\n")
+
+			fmt.Printf(" Id: %-7s \n", t.ID.String())
+			fmt.Printf(" Description: %-25s\n", t.Description)
+			fmt.Printf(" Status: %-10s\n ", t.Status)
+		}
+
+		break
+	case "mark-in-progress", "-mp":
+		if len(args) < 2 {
+			fmt.Println("Error: Correct usage: mark-in-progress <id>")
+			return
+		}
+		idInput := args[1]
+
+		success, msg := service.TaskUpdate("", "mark-in-progress", uuid.MustParse(idInput))
+
+		if !success {
+			fmt.Printf("%s\n", msg)
+		} else {
+			fmt.Printf("%s\n", msg)
+		}
+	case "mark-done", "-md":
+		if len(args) < 2 {
+			fmt.Println("Error: Correct usage: mark-done <id>")
+			return
+		}
+		idInput := args[1]
+
+		success, msg := service.TaskUpdate("", "mark-done", uuid.MustParse(idInput))
+
+		if !success {
+			fmt.Printf("%s\n", msg)
+		} else {
+			fmt.Printf("%s\n", msg)
+		}
+	case "delete", "-d":
+		if len(args) < 2 {
+			fmt.Println("Error: Correct usage: delete <id>")
+			return
+		}
+		idInput := args[1]
+
+		success, msg := service.TaskDelete(uuid.MustParse(idInput))
+
+		if !success {
+			fmt.Printf("%s\n", msg)
+		} else {
+			fmt.Printf("%s\n", msg)
+		}
+		break
 	case "help", "-h":
 		globalHelp()
 		break
@@ -44,7 +137,7 @@ func globalHelp() {
 	fmt.Println("Use: task <command> [arguments]")
 	fmt.Println("\n Enable commands:")
 	fmt.Println("  add                 Create new task.")
-	fmt.Println("  update              Update existing task.(Example: description id)")
+	fmt.Println("  update              Update existing task.(Example: id new description)")
 	fmt.Println("  delete              Delete a task by ID. (Example: delete id)")
 	fmt.Println("  mark-in-progress    Mark task in status progress. (Example: mark-in-progress id)")
 	fmt.Println("  mark-done           Mark task as done. (Example: mark-done id)")
